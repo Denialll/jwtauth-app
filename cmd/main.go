81f42449519f00571ff355e3bca014847a6fd25c
Your Name <den4ic369@gmail.com>
2023-09-14 15:14:32 +0300

    sc

--- cmd/main.go
+++ cmd/main.go
@@ -5,17 +5,26 @@ import (
 	"github.com/Denialll/jwtauth-app/pkg/handler"
 	"github.com/Denialll/jwtauth-app/pkg/repository"
 	"github.com/Denialll/jwtauth-app/pkg/service"
+	"github.com/spf13/viper"
 	"log"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error init")
+	}
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
-	handlers = new(handler.Handler)
 	srv := new(jwtauth_app.Server)
-	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("portc"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
